114ab9da5de3baa217f2a57fcadfd37db2103a5b
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-19 04:23:43 +0530

    helper func added to parse image from sanity cdn

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Nav from "./components/common/Nav";
+import { client } from "@/lib/sanity.client";
 
-function Store() {
+function Store({ products }: any) {
   return (
     <main className="h-fit m-0">
       <div className="max-w-[1600px] mx-auto">
         <Nav />
+        <div>{products?.map((product: any) => console.log(product))}</div>
       </div>
     </main>
   );
 }
 
+export const getServerSideProps = async () => {
+  const productQuery = `*[_type == "product"]`;
+  const products = await client.fetch(productQuery);
+  return {
+    props: { products },
+  };
+};
 export default Store;
