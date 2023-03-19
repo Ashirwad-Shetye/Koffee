e5b0b977d064e5fc45a5a68874b655fa33336a15
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-19 13:53:32 +0530

    image loaind error fixed

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import Image from "next/image";
 import Nav from "./components/common/Nav";
 import { client } from "@/lib/sanity.client";
+import urlFor from "@/lib/urlFor";
 
 function Store({ products }: any) {
   return (
     <main className="h-fit m-0">
       <div className="max-w-[1600px] mx-auto">
         <Nav />
-        <div>{products?.map((product: any) => console.log(product))}</div>
+        <div>
+          {products?.map((product: any) => (
+            <div key={product.id}>
+              <div className="relative w-full h-96">
+                <Image
+                  src={urlFor(product.image[0]).url()}
+                  alt="product image"
+                  fill
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
